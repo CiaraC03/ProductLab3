1861df957087195dd7c2401914183cfa4c788136
G00402357 <G00402357@atu.ie>
2023-02-16 12:45:50 +0000

    new class created

--- src/main/java/ie/atu/Producttv6/Book.java
+++ src/main/java/ie/atu/Producttv6/Book.java
@@ -1,4 +1,17 @@
 package ie.atu.Producttv6;
 
-public class Book {
+public class Book extends Product{
+    public String author;
+
+    public Book(){
+        author = "";
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
