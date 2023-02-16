a38af5f428419ad6e66e23b6d494d6445a041580
G00402357 <G00402357@atu.ie>
2023-02-16 11:36:02 +0000

    new code  created in second class

--- src/main/java/ie/atu/Producttv6/Dog.java
+++ src/main/java/ie/atu/Producttv6/Dog.java
@@ -4,4 +4,35 @@ public class Dog extends Animal{
     private String vaccination;
     private String shedding;
     private String neutered;
+
+    public Dog(){
+
+    }
+
+    public String getVaccination() {
+        return vaccination;
+    }
+
+    public void setVaccination(String vaccination) {
+        this.vaccination = vaccination;
+    }
+
+    public String getShedding() {
+        return shedding;
+    }
+
+    public void setShedding(String shedding) {
+        this.shedding = shedding;
+    }
+
+    public String getNeutered() {
+        return neutered;
+    }
+
+    public void setNeutered(String neutered) {
+        this.neutered = neutered;
+    }
 }
+
+
+
